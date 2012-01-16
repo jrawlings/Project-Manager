930e31627401db409e48d5b5a66a003ea07d818d
Joe Rawlings <joseph.rawlings@gmail.com>
2012-01-15 22:31:16 -0700

    Minor update for the night
    Going to sleep :)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  
+  def link_to_icon(icon_name, url_or_object, options={})
+    
+    link_to(image_tag("icons/#{icon_name}.png"),
+      url_or_object,
+      options)
+  end
+  
 end
